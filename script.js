56a91b7a09d10a59cfcf702ee74d676ccd3c0810
ChrisJones86 <110697929+ChrisJones86@users.noreply.github.com>
2023-03-23 16:09:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,76 @@
 var state = {
     balance: 10000,
     income: 400000,
-    expence: 1000,
+    expense: 1000,
     transactions: [
-        { id: uniqueId(), name: 'Salary', amount: 25000, type: 'income' },
-        { id: uniqueId(), name: 'Pay Morgage', amount: 3500, type: 'expence'},
-        { id: uniqueId(), name: 'Buy Fishing Gear', amount: 2000, type: 'expence'},
+        { id: uniqueId(), name: 'Pay Check', amount: 25000, type: 'income' },
+        { id: uniqueId(), name: 'Pay Morgage', amount: 3500, type: 'expense'},
+        { id: uniqueId(), name: 'Buy Fishing Gear', amount: 2000, type: 'expense'},
     ]
 }
 
 var balanceEl = document.querySelector('#balance');
 var incomeEl = document.querySelector('#income');
-var expenceEl = document.querySelector('#expence');
-var transactionEl = document.querySelector('#transaction');
+var expenseEl = document.querySelector('#expense');
+var transactionsEl = document.querySelector('#transaction');
 var incomeBtnEl = document.querySelector('#incomeBtn');
-var expenceBtnEl = document.querySelector('#expenceBtn');
+var expenseBtnEl = document.querySelector('#expenseBtn');
 var nameInputEl = document.querySelector('#name');
 var amountInputEl = document.querySelector('#amount');
 
 function init() {
     updateState();
-    initListners();
+    initListeners();
 }
 
-function uniquId(){
+function uniqueId() {
     return Math.round(Math.random() * 1000000);
 }
 
-function initListners() {
+function initListeners() {
     incomeBtnEl.addEventListener('click', onAddIncomeClick);
-    expenceBtnEl.addEventListener('click', onAddExpenceClick);
-
+    expenseBtnEl.addEventListener('click', onAddExpenseClick);
 }
 
 function onAddIncomeClick() {
     addTransaction(nameInputEl.value, amountInputEl.value, 'income');
 }
 
-function onAddExpenceClick() {
-    addTransaction(nameInputEl.value, amountInputEl.value, 'expence');
+function onAddExpenseClick() {
+    addTransaction(nameInputEl.value, amountInputEl.value, 'expense');
 }
 
-function addTransaction(name, amount, type){
-    if(name !== '' && amount !== '') {
+function addTransaction(name, amount, type) {
+    if (name !== '' && amount !== '') {
         var transaction = { 
-            id: uniquId(),
+            id: uniqueId(),
             name: name, 
             amount: parseInt(amount), 
-            type: type 
+            type: type
         };
 
         state.transactions.push(transaction);
 
         updateState();
-    }else {
-        alert('Please enter calid data');
+    } else {
+        alert('Please enter valid data');
     }
 
     nameInputEl.value = '';
     amountInputEl.value = '';
-
 }
 
 function onDeleteClick(event) {
-    var id = parseInt (event.target.getAttribute('data-id'));
-    var deleteIntex;
+    var id = parseInt(event.target.getAttribute('data-id'));
+    var deleteIndex;
     for (var i = 0; i < state.transactions.length; i++) {
-        if (state.transactions[i].id === id){
-            deleteIntex = i;
+        if (state.transactions[i].id === id) {
+            deleteIndex = i;
             break;
         }
     }
 
-    state.transactions.splice(deleteIntex, 1);
+    state.transactions.splice(deleteIndex, 1);
 
     updateState();
 }
@@ -80,57 +78,57 @@ function onDeleteClick(event) {
 function updateState() {
     var balance = 0,
         income = 0,
-        expence = 0,
+        expense = 0,
         item;
 
     for (var i = 0; i < state.transactions.length; i++) {
         item = state.transactions[i];
 
-        if(item.type === 'income') {
+        if (item.type === 'income') {
             income += item.amount;
-        }else if (item.type === 'expence') {
-            expence += item.amount;
+        } else if (item.type === 'expense') {
+            expense += item.amount;
         }
     }
 
-    balance = income - expence;
+    balance = income - expense;
 
-    console.log(balance, income, expence);
     state.balance = balance;
     state.income = income;
-    state.expence = expence;
+    state.expense = expense;
+
     render();
 }
 
-    function render() {
-        balanceEl.innerHTML = '$${state.balance}';
-        incomeEl.innerHTML = '$${state.income}';
-        expenceEl.innerHTML = '$${state.expence}';
+function render() {
+    balanceEl.innerHTML = `$${state.balance}`;
+    incomeEl.innerHTML = `$${state.income}`;
+    expenseEl.innerHTML = `$${state.expense}`;
 
-        var transactionEl, containerEl, amountEl, item, btnEl;
+    var transactionEl, containerEl, amountEl, item, btnEl;
 
-        transactionsEl.innerHTML = '';
+    transactionsEl.innerHTML = '';
 
     for (var i = 0; i < state.transactions.length; i++) {
         item = state.transactions[i];
         transactionEl = document.createElement('li');
-        transactionEl.append(state.transactions[i].name);
+        transactionEl.append(item.name);
 
         transactionsEl.appendChild(transactionEl);
 
         containerEl = document.createElement('div');
         amountEl = document.createElement('span');
         if (item.type === 'income') {
-            amountEl.classList.add('income-amt')
-        } else if (item.type === 'expence') {
-            amountEl.classList.add('expence-amt')
+            amountEl.classList.add('income-amt');
+        } else if (item.type === 'expense') {
+            amountEl.classList.add('expense-amt');
         }
-        amountEl.innerHTML = '$${item.amount}';
+        amountEl.innerHTML = `$${item.amount}`;
 
         containerEl.appendChild(amountEl);
 
-        btnEl = document.appendChild('button');
-        btnEL.setAttribute('data-id', item.id)
+        btnEl = document.createElement('button');
+        btnEl.setAttribute('data-id', item.id);
         btnEl.innerHTML = 'X';
 
         btnEl.addEventListener('click', onDeleteClick);
@@ -141,4 +139,4 @@ function updateState() {
     }
 }
 
-init()
+init();
